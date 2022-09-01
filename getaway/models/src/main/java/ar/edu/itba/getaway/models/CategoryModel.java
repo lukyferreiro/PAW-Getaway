3e8f46bc218f188c1c52ca4a15497edc90bb0084
rgomezkiss <romgomez@itba.edu.ar>
2022-09-01 00:14:42 -0300

    First version of CategoryModel

---
+++ getaway/models/src/main/java/ar/edu/itba/getaway/models/CategoryModel.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.getaway.models;
+
+public class CategoryModel {
+    private final long categoryId;
+    private final String categoryName;
+
+    public CategoryModel(long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public long getId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return categoryName;
+    }
+}
