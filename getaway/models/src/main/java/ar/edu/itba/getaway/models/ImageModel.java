336cc988f06ed6d2c21d16534041703b072ae818
lukyferreiro <lferreiro@itba.edu.ar>
2022-09-02 01:28:41 -0300

    Fix error in ImageModel

--- getaway/models/src/main/java/ar/edu/itba/getaway/models/ImageModel.java
+++ getaway/models/src/main/java/ar/edu/itba/getaway/models/ImageModel.java
@@ -2,17 +2,28 @@ package ar.edu.itba.getaway.models;
 
 public class ImageModel {
     private final long imageId;
-    private final MultipartFile image;
+    private final long experienceId;
+    private boolean isCover;
 
-    public ImageModel(long imageId, MultipartFile image ) {
+    public ImageModel(long imageId, long experienceId, boolean isCover ) {
         this.imageId = imageId;
+        this.experienceId = experienceId;
+        this.isCover = isCover;
     }
 
     public long getId() {
         return imageId;
     }
 
-    public MultipartFile getImage() {
-        return image;
+    public long getExperienceId() {
+        return experienceId;
+    }
+
+    public boolean isCover() {
+        return isCover;
+    }
+
+    public void setIsCover(boolean isCover) {
+        this.isCover = isCover;
     }
 }
