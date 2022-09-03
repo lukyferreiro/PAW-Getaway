c508159e65da3310624eff93a93f5e6cd2c4d79b
rgomezkiss <romgomez@itba.edu.ar>
2022-09-03 10:44:13 -0300

    Added IF NOT EXISTS sentences to schema.sql and fixed minor problems

--- getaway/persistence/src/main/resources/schema.sql
+++ getaway/persistence/src/main/resources/schema.sql
@@ -22,15 +22,15 @@ CREATE TABLE IF NOT EXISTS roles
     UNIQUE (roleName)
     );
 
-CREATE TABLE countries
+CREATE TABLE IF NOT EXISTS countries
 (
     countryId SERIAL NOT NULL,
     countryName VARCHAR(255) NOT NULL,
     PRIMARY KEY (countryId),
     UNIQUE (countryName)
-);
+    );
 
-CREATE TABLE cities
+CREATE TABLE IF NOT EXISTS cities
 (
     cityId SERIAL NOT NULL,
     cityName VARCHAR(255) NOT NULL,
@@ -38,31 +38,31 @@ CREATE TABLE cities
     PRIMARY KEY (cityId),
     FOREIGN KEY (countryId) REFERENCES countries (countryId) ON DELETE CASCADE,
     UNIQUE (countryId)
-);
+    );
 
-CREATE TABLE images
+CREATE TABLE IF NOT EXISTS images
 (
     imgId SERIAL,
-    image BLOB,
-    PRIMARY KEY (imgId),
-);
+    image BYTEA,
+    PRIMARY KEY (imgId)
+    );
 
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     userId SERIAL,
     userName VARCHAR(50) NOT NULL,
     userSurname VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     imgId INT,
-    role_id INT NOT NULL,
+    roleId INT NOT NULL,
     password VARCHAR(255),
     PRIMARY KEY (userId),
     UNIQUE(email),
     FOREIGN KEY (roleId) REFERENCES roles (roleId) ON DELETE CASCADE,
     FOREIGN KEY (imgId) REFERENCES images (imgId)
-);
+    );
 
-CREATE TABLE experiences
+CREATE TABLE IF NOT EXISTS experiences
 (
     experienceId SERIAL,
     experienceName VARCHAR(255) NOT NULL,
@@ -74,29 +74,29 @@ CREATE TABLE experiences
     userId INT NOT NULL,
     PRIMARY KEY (experienceId),
     FOREIGN KEY (cityId) REFERENCES cities (cityId) ON DELETE CASCADE,
-    FOREIGN KEY (categoryId) REFERENCES categories (categoryId) ON DELETE ,
+    FOREIGN KEY (categoryId) REFERENCES categories (categoryId) ON DELETE CASCADE,
     FOREIGN KEY (userId) REFERENCES users (userId) ON DELETE CASCADE
-);
+    );
 
-CREATE TABLE imagesExperiences
+CREATE TABLE IF NOT EXISTS imagesExperiences
 (
     imgId INT NOT NULL,
     experienceId INT NOT NULL,
     isCover BOOLEAN NOT NULL,
     PRIMARY KEY (imgId),
-    FOREIGN KEY (imgId) REFERENCES images (imgId) ON DELETE CASCADE
-        FOREIGN KEY (experienceId) REFERENCES experiences (experienceId) ON DELETE CASCADE
-);
+    FOREIGN KEY (imgId) REFERENCES images (imgId) ON DELETE CASCADE,
+    FOREIGN KEY (experienceId) REFERENCES experiences (experienceId) ON DELETE CASCADE
+    );
 
-CREATE TABLE tagExperience
+CREATE TABLE IF NOT EXISTS tagExperience
 (
     tagId INT NOT NULL,
     experienceId INT NOT NULL,
     FOREIGN KEY (tagId) REFERENCES tags (tagId) ON DELETE CASCADE,
     FOREIGN KEY (experienceId) REFERENCES experiences (experienceId) ON DELETE CASCADE
-);
+    );
 
-CREATE TABLE reviews
+CREATE TABLE IF NOT EXISTS reviews
 (
     reviewId SERIAL,
     description TEXT,
@@ -105,4 +105,4 @@ CREATE TABLE reviews
     reviewDate DATE NOT NULL,
     PRIMARY KEY (reviewId),
     FOREIGN KEY (experienceId) REFERENCES experiences (experienceId) ON DELETE CASCADE
-);
+    );
