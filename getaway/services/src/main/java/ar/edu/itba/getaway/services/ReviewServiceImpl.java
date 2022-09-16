2e8733bc98d31a41d1e5c10b527e58ee46ebb387
Agustina Ortu <aortu@itba.edu.ar>
2022-09-15 23:58:31 -0300

    Adding dao and services for reviews

---
+++ getaway/services/src/main/java/ar/edu/itba/getaway/services/ReviewServiceImpl.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.getaway.services;
+
+import ar.edu.itba.getaway.models.ReviewModel;
+import ar.edu.itba.getaway.persistence.ReviewDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+public class ReviewServiceImpl implements ReviewService{
+    @Autowired
+    private ReviewDao reviewDao;
+
+
+    @Override
+    public ReviewModel create(String title, String description, long score, long experienceId, Date reviewDate) {
+        return reviewDao.create(title,description,score,experienceId,reviewDate);
+    }
+
+    @Override
+    public List<ReviewModel> getReviewsFromId(long experienceId) {
+        return reviewDao.getReviewsFromId(experienceId);
+    }
+}
