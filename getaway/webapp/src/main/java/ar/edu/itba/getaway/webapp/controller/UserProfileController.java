26a3119ed1ed73b9253689bd650a02d2b4f3db83
rgomezkiss <romgomez@itba.edu.ar>
2022-09-26 17:23:57 -0300

    Added more info to my_profile page. Added template to show profile image in navbar

--- getaway/webapp/src/main/java/ar/edu/itba/getaway/webapp/controller/UserProfileController.java
+++ getaway/webapp/src/main/java/ar/edu/itba/getaway/webapp/controller/UserProfileController.java
@@ -41,6 +41,7 @@ public class UserProfileController {
 
         final UserModel user = userService.getUserByEmail(principal.getName()).orElseThrow(UserNotFoundException::new);
         mav.addObject("user", user);
+        mav.addObject("hasImage", imageService.getImgById(user.getProfileImageId()).get().getImage() != null);
         return mav;
     }
 
