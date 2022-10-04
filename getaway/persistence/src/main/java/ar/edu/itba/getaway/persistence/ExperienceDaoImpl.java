13976822d540d8e3f294dc778206f6507b212f14
Agustina Ortu <aortu@itba.edu.ar>
2022-10-04 09:49:08 -0300

    Rewvies from higher to lower

--- getaway/persistence/src/main/java/ar/edu/itba/getaway/persistence/ExperienceDaoImpl.java
+++ getaway/persistence/src/main/java/ar/edu/itba/getaway/persistence/ExperienceDaoImpl.java
@@ -195,7 +195,7 @@ public class ExperienceDaoImpl implements ExperienceDao {
 
     @Override
     public List<ExperienceModel> listExperiencesByBestRanked(Long categoryId){
-        final String query = "SELECT experiences.experienceId, experienceName, address, experiences.description, email, siteUrl, price, cityId, categoryId, experiences.userId FROM experiences LEFT JOIN reviews ON experiences.experienceid = reviews.experienceid WHERE categoryid = ? GROUP BY experiences.experienceid ORDER BY avg(score) ASC ";
+        final String query = "SELECT experiences.experienceId, experienceName, address, experiences.description, email, siteUrl, price, cityId, categoryId, experiences.userId FROM experiences LEFT JOIN reviews ON experiences.experienceid = reviews.experienceid WHERE categoryid = ? GROUP BY experiences.experienceid ORDER BY AVG(COALESCE(score,0)) DESC ";
         LOGGER.debug("Executing query: {}", query);
         return jdbcTemplate.query(query, new Object[]{categoryId}, EXPERIENCE_MODEL_ROW_MAPPER);
     }
