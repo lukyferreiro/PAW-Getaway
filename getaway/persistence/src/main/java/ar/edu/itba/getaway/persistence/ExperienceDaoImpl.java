d40c5782dc9701188b7b7cb374a37cf13b718e7a
tomasalvarez <talvarez@balanz.com>
2022-10-28 11:24:49 -0300

    Experiences can now be also searched by description and address

--- getaway/persistence/src/main/java/ar/edu/itba/getaway/persistence/ExperienceDaoImpl.java
+++ getaway/persistence/src/main/java/ar/edu/itba/getaway/persistence/ExperienceDaoImpl.java
@@ -135,7 +135,7 @@ public class ExperienceDaoImpl implements ExperienceDao {
         else {
             orderQuery = OrderByModel.OrderByRankDesc.getSqlQuery();
         }
-        final TypedQuery<ExperienceModel> query = em.createQuery("SELECT exp FROM ExperienceModel exp WHERE LOWER(exp.experienceName) LIKE LOWER(CONCAT('%', :name,'%')) AND (exp.observable=true OR exp.user=:user)" + orderQuery, ExperienceModel.class);
+        final TypedQuery<ExperienceModel> query = em.createQuery("SELECT exp FROM ExperienceModel exp WHERE LOWER(exp.experienceName) LIKE LOWER(CONCAT('%', :name,'%')) OR LOWER(exp.description) LIKE LOWER(CONCAT('%', :name,'%')) OR LOWER(exp.address) LIKE LOWER(CONCAT('%', :name,'%')) AND (exp.observable=true OR exp.user=:user)" + orderQuery, ExperienceModel.class);
         query.setParameter("name", name);
         query.setParameter("user", user);
         query.setFirstResult((page - 1) * pageSize);
@@ -145,7 +145,7 @@ public class ExperienceDaoImpl implements ExperienceDao {
 
     @Override
     public Long getCountByName(String name, UserModel user) {
-        final TypedQuery<Long> query = em.createQuery("SELECT COUNT(exp) FROM ExperienceModel exp WHERE LOWER(exp.experienceName) LIKE LOWER(CONCAT('%', :name,'%')) AND (exp.observable=true OR exp.user=:user)", Long.class);
+        final TypedQuery<Long> query = em.createQuery("SELECT COUNT(exp) FROM ExperienceModel exp WHERE LOWER(exp.experienceName) LIKE LOWER(CONCAT('%', :name,'%')) OR LOWER(exp.description) LIKE LOWER(CONCAT('%', :name,'%')) OR LOWER(exp.address) LIKE LOWER(CONCAT('%', :name,'%')) AND (exp.observable=true OR exp.user=:user)", Long.class);
         query.setParameter("name", name);
         query.setParameter("user", user);
         return query.getSingleResult();
