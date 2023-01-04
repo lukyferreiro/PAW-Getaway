70fcf4e832c8533883ed0892550b086811dd95ff
Agustina Ortu <aortu@itba.edu.ar>
2023-01-04 15:44:17 -0300

    Adding validators

---
+++ getaway/webapp/src/main/java/ar/edu/itba/getaway/webapp/dto/validations/ImageTypeConstraint.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.getaway.webapp.dto.validations;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = {InvalidImageTypeValidator.class})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ImageTypeConstraint {
+
+    String message() default "Invalid image type";
+
+    String[] contentType();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
