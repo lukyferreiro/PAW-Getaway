f9a59859768638d66ef35e717ef0b52f4e752554
lukyferreiro <lferreiro@itba.edu.ar>
2023-07-03 14:58:28 -0300

    Fixed refresh in browser :)

--- getaway/frontend/src/common/index.js
+++ getaway/frontend/src/common/index.js
@@ -1,7 +1,7 @@
 export const paths = {
     // LOCAL_BASE_URL : '/',
     // BASE_URL: 'http://localhost:8080/webapp_war/api',
-    LOCAL_BASE_URL : '/paw-2022b-1',
+    LOCAL_BASE_URL : '/paw-2022b-1/',
     BASE_URL: 'http://pawserver.it.itba.edu.ar/paw-2022b-1/api',
     EXPERIENCES: '/experiences',
     LOCATION: '/location',
