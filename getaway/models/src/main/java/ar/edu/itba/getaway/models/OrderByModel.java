fc02fbe262b4a561c43c750778953daf308f016f
lukyferreiro <lferreiro@itba.edu.ar>
2023-07-11 20:46:29 -0300

    Fix order of price when its null

--- getaway/models/src/main/java/ar/edu/itba/getaway/models/OrderByModel.java
+++ getaway/models/src/main/java/ar/edu/itba/getaway/models/OrderByModel.java
@@ -1,14 +1,12 @@
 package ar.edu.itba.getaway.models;
 
-import java.util.Comparator;
-
 public enum OrderByModel {
     OrderByRankAsc("ORDER BY exp.averageScore ASC"),
     OrderByRankDesc("ORDER BY exp.averageScore DESC"),
     OrderByAZ("ORDER BY exp.experienceName ASC"),
     OrderByZA("ORDER BY exp.experienceName DESC"),
-    OrderByLowPrice("ORDER BY exp.price ASC"),
-    OrderByHighPrice("ORDER BY exp.price DESC"),
+    OrderByLowPrice("ORDER BY exp.price ASC NULLS FIRST"),
+    OrderByHighPrice("ORDER BY exp.price DESC NULLS LAST"),
     OrderByViewAsc("ORDER BY exp.views ASC"),
     OrderByViewDesc("ORDER BY exp.views DESC");
 
