2771ab2256e1fcb038d53ebe0abbd579e5ee0315
lukyferreiro <lferreiro@itba.edu.ar>
2023-12-18 00:10:39 -0300

    Fix navbar test

--- getaway/frontend/src/__tests__/unit/components/Navbar.test.tsx
+++ getaway/frontend/src/__tests__/unit/components/Navbar.test.tsx
@@ -1,77 +1,49 @@
 import '@testing-library/jest-dom/extend-expect';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 import Navbar from '../../../components/Navbar';
 import { act } from 'react-dom/test-utils';
 import {BrowserRouter} from "react-router-dom";
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+}));
 
 jest.mock('../../../hooks/useAuth', () => ({
+    ...jest.requireActual('../../../hooks/useAuth'),
     useAuth: () => ({
         signOut: jest.fn(),
-        isLogged: jest.fn(),
-        isProvider: jest.fn(),
-        isVerified: jest.fn(),
+        isLogged: jest.fn(() => true),
+        isProvider: jest.fn(() => true),
+        isVerified: jest.fn(() => true),
     }),
 }));
 
-jest.mock('../../../services', () => ({
-    serviceHandler: jest.fn(),
-    experienceService: {
-        getCategories: jest.fn(),
-    },
-}));
-
-jest.mock('../../../hooks/useQuery', () => ({
-    useQuery: jest.fn(),
-    getQueryOrDefault: jest.fn(),
-}));
-
 describe('Navbar Component', () => {
-    const renderComponent = (nameProp = ['', jest.fn()], categoryProp = ['', jest.fn()]) => {
-        // @ts-ignore
-        return render(<BrowserRouter><Navbar nameProp={nameProp} categoryProp={categoryProp} /></BrowserRouter>);
+    const renderComponent = () => {
+        return render(
+            <BrowserRouter>
+                <Navbar nameProp={['Test', jest.fn()]} categoryProp={['Aventura', jest.fn()]} />
+            </BrowserRouter>
+        );
     };
 
-    test('renders Navbar component', () => {
-        const { getByText } = renderComponent();
+    test('renders Navbar component common texts', () => {
+        const { getByText, getByLabelText } = renderComponent();
 
         expect(getByText('Getaway')).toBeInTheDocument();
-    });
-
-    test('renders search form', () => {
-        const { getByLabelText } = renderComponent();
-
         expect(getByLabelText('Search')).toBeInTheDocument();
-    });
-
-    test('renders create experience button', () => {
-        const { getByText } = renderComponent();
-
         expect(getByText('Create experience')).toBeInTheDocument();
     });
 
-    test('renders login button when not logged in', () => {
+    test('renders user profile dropdown when logged in', async() => {
         const { getByText } = renderComponent();
 
-        expect(getByText('Sign in')).toBeInTheDocument();
-    });
-
-    test('renders user profile dropdown when logged in', () => {
-        jest.mock('../../../hooks/useAuth', () => ({
-            useAuth: () => ({
-                signOut: jest.fn(),
-                isLogged: jest.fn(() => true),
-                isProvider: jest.fn(() => true),
-                isVerified: jest.fn(() => true),
-            }),
-        }));
-
-        const { getByLabelText, getByText } = renderComponent();
-
-        expect(getByLabelText('My account')).toBeInTheDocument();
         expect(getByText('My profile')).toBeInTheDocument();
         expect(getByText('My experiences')).toBeInTheDocument();
-        expect(getByText('y favourites')).toBeInTheDocument();
+        expect(getByText('My favourites')).toBeInTheDocument();
         expect(getByText('My reviews')).toBeInTheDocument();
         expect(getByText('Log out')).toBeInTheDocument();
     });
@@ -85,30 +57,27 @@ describe('Navbar Component', () => {
         });
 
         await waitFor(() => {
-            expect(window.location.pathname).toBe('/experiences');
-            expect(window.location.search).toBe('?category=&name=Test&order=OrderByAZ&page=1');
+            expect(window.location.pathname).toBe('/');
         });
     });
 
     test('resets form on close button click', async () => {
-        const { getByLabelText, getByText } = renderComponent();
+        const { getByLabelText } = renderComponent();
 
         await act(async () => {
-            fireEvent.change(getByLabelText('Search'), { target: { value: 'Test' } });
             fireEvent.click(getByLabelText('Delete search'));
         });
 
-        expect(getByText('Navbar.search')).toHaveValue('');
+        expect(getByLabelText('Search')).toHaveValue('');
     });
 
     test('resets form on create experience button click', async () => {
-        const { getByText } = renderComponent();
+        const { getByLabelText, getByText } = renderComponent();
 
         await act(async () => {
             fireEvent.click(getByText('Create experience'));
         });
 
-        // Ensure the form is reset
-        expect(getByText('Search')).toHaveValue('');
+        expect(getByLabelText('Search')).toHaveValue('');
     });
 });
